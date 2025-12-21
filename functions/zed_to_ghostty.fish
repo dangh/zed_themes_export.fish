1b8b9d5644ab20236ffa5f68fbea7b1de604d1d9
Dang <huynhminhdang@gmail.com>
2025-12-22 02:15:45 +0700

    update github url

--- functions/zed_to_ghostty.fish
+++ functions/zed_to_ghostty.fish
@@ -2,7 +2,7 @@ function zed_to_ghostty
     set themes ayu gruvbox one
     set json (mktemp)
     for theme in $themes
-        curl -s https://raw.githubusercontent.com/zed-industries/zed/main/assets/themes/$theme/$theme.json -o $json
+        curl -sL https://github.com/zed-industries/zed/raw/refs/heads/main/assets/themes/$theme/$theme.json -o $json
         for i in (seq 0 (jq '.themes | length - 1' $json))
             jq -r ".themes[$i].name" $json | string replace -a ' ' '' | read name
             set name Zed$name
